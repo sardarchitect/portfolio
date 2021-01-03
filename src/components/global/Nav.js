8142a139376112badb4a0c7f1a12bd2f5ead1fe3
Arvinder Singh <sardarchitect@gmail.com>
2021-01-03 15:10:07 -0500

    Updated CV, Added Blog Page

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../stylesheets/_nav.scss";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Nav = () => {
   const location = useLocation();
@@ -9,9 +9,9 @@ export const Nav = () => {
   if (location.pathname.includes("cv")) {
     return (
       <nav>
-        <a href="/">HOME</a>
+        <Link to="/">HOME</Link>
         <a href="#cv">CV</a>
-        <a href="/blog">BLOG</a>
+        <Link to="/blog">BLOG</Link>
         <a href="#contact">CONTACT</a>
       </nav>
     );
@@ -20,9 +20,9 @@ export const Nav = () => {
   if (location.pathname.includes("projects")) {
     return (
       <nav>
-        <a href="/">HOME</a>
+        <Link to="/">HOME</Link>
         <a href="#work">SELECTED WORK</a>
-        <a href="/blog">BLOG</a>
+        <Link to="/blog">BLOG</Link>
         <a href="#contact">CONTACT</a>
       </nav>
     );
@@ -31,9 +31,9 @@ export const Nav = () => {
   if (location.pathname.includes("blog")) {
     return (
       <nav>
-        <a href="/">HOME</a>
-        <a href="/#work">SELECTED WORK</a>
-        <a href="/cv">CV</a>
+        <Link to="/">HOME</Link>
+        <Link to="/#work">SELECTED WORK</Link>
+        <Link to="/cv">CV</Link>
         <a href="#contact">CONTACT</a>
       </nav>
     );
@@ -43,8 +43,8 @@ export const Nav = () => {
   return (
     <nav>
       <a href="#work">WORK</a>
-      <a href="/cv">CV</a>
-      <a href="/blog">BLOG</a>
+      <Link to="/cv">CV</Link>
+      <Link to="/blog">BLOG</Link>
       <a href="#contact">CONTACT</a>
     </nav>
   );
