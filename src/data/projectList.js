3426c80d8aaa8ee5da22affbfdcc8b231d2b38e9
Arvinder Singh <sardarchitect@gmail.com>
2021-05-09 10:18:41 -0400

    refactor

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -48,7 +48,7 @@ export const projects = [
     text: [
       `This project started through an observation  that certain problems around the world such as overpopulation, climate change, political strife , nuclear threat, and the increasing economic gap, are swiftly reaching a breaking point. No previous generation has quite faced these large-scale, existential threats that not only affect our own species, but also this planet at large.`,
       `Seek helps users to identify the scale and context of important, urgent issues going on around the world in a easy-to-understand filtration process. It allows users to see the problem through multiple ways: visual trend graphs, short descriptions, and chronological news articles.`,
-      `Solve gives users the ability to take action. By associating efficient groups and organizations to the issues, Solve helps people to get directly involved with the solution (through multiple ways of engagement). To help users decide which nonprofits they would like to support , Solve shows rankings of nonprofits based on different, credible watchdog groups, and tries to promote those that need the most public attention. `,
+      `Solve gives users the ability to take action. By associating efficient groups and organizations to the issues, Solve helps people to get directly involved with the solution (through multiple ways of engagement). To help users decide which nonprofits they would like to support , Solve shows rankings of nonprofits based on different, credible watchdog groups, and tries to promote those that need the most public attention.`,
     ],
     images: [
       { src: "/project-gallery/seek2solve/seek2solve-1.png", caption: "Seek" },
