2934d6c73d191b1b45fb2cda145a291fda1a4dab
Arvinder Singh <sardarchitect@gmail.com>
2021-06-30 13:21:59 -0400

    added new project

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -102,6 +102,10 @@ export const projects = [
         src: "/project-gallery/framework-for-frameworks/posters.png",
         caption: "Renderings showing the process of change within a pueblo, and the larger surroundings",
       },
+      {
+        src: "/project-gallery/framework-for-frameworks/setup.jpg",
+        caption: "Layout of my presentation",
+      },
       ],
   },
   {
@@ -175,6 +179,29 @@ export const projects = [
       {
         src: "/project-gallery/baker-college/A.501_CCD003.png",
         caption: "Working drawings for reference only",
+      },
+    ],
+  },
+  {
+    category: "architecture",
+    projectId: "the-dot",
+    title: "The dot at Ferndale",
+    year: 2019,
+    studio: "Edge Design Associates, Inc.",
+    role: "Architectural Designer I",
+    site: "Ferndale, Michigan",
+  team: "Daniel Mooney | Heather Buffone | Arvinder Singh",
+    award: null,
+    thumbnail: "/thumbnail/thumbnail-the-dot.jpg",
+    text: [
+      `The dot (The Development on Troy) is a mixed-use parking, retail, and office development in Downtown Ferndale.`,
+      `It is a mixed-use building with 2 floors of office space, 1 floor for retail shops, and 5 levels of parking, right in the heart of downtown Ferndale`,
+      `My work within this project had to do with construction management, responding to contractor RFIs, and drafting office layouts.`
+    ],
+    images: [
+      {
+        src: "/project-gallery/the-dot/the-dot.jpg",
+        caption: "Computer rendering",
       },
     ],
   },
